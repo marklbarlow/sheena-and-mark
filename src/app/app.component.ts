7e7420294647864ee2b0ed291f1985ab07c71e25
marklbarlow <Mark@Hibiki.connect>
2017-09-02 00:06:58 +0100

    Initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
+  animations: [
+    trigger('titleAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('2.0s 0.5s ease-out', style({ opacity: 1 }))
+      ])
+    ]),
+    trigger('subtitleAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('6.0s 2.5s ease-out', style({ opacity: 1 }))
+      ])
+    ])
+  ],
   selector: 'app-root',
+  styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
