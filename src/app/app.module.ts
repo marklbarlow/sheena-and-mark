b88405b2e32a169e170dd24aa6174b479a60adc5
marklbarlow <mark.l.barlow@gmail.com>
2017-09-13 00:13:12 +0100

    Update styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { VenueComponent } from './venue/venue.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/venue', pathMatch: 'full' },
-  { path: 'venue', component: VenueComponent },
+  // { path: '', redirectTo: '/venue', pathMatch: 'full' },
+  // { path: 'venue', component: VenueComponent },
+  { path: '', component: VenueComponent },
 ];
 
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [
     AppComponent,
     VenueComponent
@@ -22,8 +25,9 @@ const routes: Routes = [
     BrowserAnimationsModule,
     FlexLayoutModule,
     RouterModule.forRoot(routes),
+    MDBBootstrapModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule { }
